a9787a2172ea31037f90e3023a8e8d3f98c1adde
Dirtycreep <Aram0070@icloud.com>
2019-02-18 16:17:24 +0400

    add class graph1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GraphProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fs = new FileStream("input.txt", FileMode.OpenOrCreate);
+            var gr = new Graph(fs);
+            gr.DeleteConnection(4, 6);
+            gr.Print();
+        }
+    }
+}
